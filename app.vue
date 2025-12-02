db3ce9d3720b4779d7be0b9832a53f5c488260b4
Tuktin Kostya <kostyatuktin@gmail.com>
2025-12-03 02:16:10 +0300

    First start site again

---
+++ app.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const hello = ref('My name is Kostya');
+</script>
+
+<template>
+  {{ hello }}
+  <TestComp />
+  <div>
+    <SharedSecondComp />
+
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
---
+++ components/Shared/SharedSecondComp.vue
@@ -0,0 +1,1 @@
+<template>Second component</template>
