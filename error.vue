5b869dd3d379cc5c086e7d0088fd5ff9dfc32bdb
Tuktin Kostya <kostyatuktin@gmail.com>
2025-12-04 02:46:41 +0300

    Create a error page

--- error.vue
+++ error.vue
@@ -4,12 +4,38 @@
     const { error } = defineProps<{
         error: NuxtError;
     }>();
+
+    function goHome() {
+        navigateTo('/');
+    } 
 </script>
 
 <template>
-    <div>
-        <h1>{{ error.statusCode }} ОШИБКА</h1>
-        <div>{{ error.statusMessage }}</div>
-        <ActionButton color="ghost">На главную</ActionButton>
+    <div class="error-wrapper">
+        <h1><b>{{ error.statusCode }}</b> ОШИБКА</h1>
+        <div  v-if="error.statusCode == 404" class="error-message">
+            Страница не найдена. Попробуйте перейти на главную страницу
+        </div>
+        <div  v-else class="error-message">{{ error.statusMessage }}</div>
+        <ActionButton color="ghost" @click="goHome">На главную</ActionButton>
     </div>
 </template>
+
+<style scoped>
+.error-message{
+    color: var(--color-dark-gray);
+    text-align: center;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 26px;
+    margin-bottom: 40px;
+}
+
+.error-wrapper{
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 24px;
+}
+</style>
