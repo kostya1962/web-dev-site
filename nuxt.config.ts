4054d1acf214f57c333ac2a22fbb8d681a7fef64
Tuktin Kostya <kostyatuktin@gmail.com>
2025-12-07 01:43:27 +0300

    Подключение внешнего CSS через CDN

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,43 +1,53 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
   routeRules: {
-    '/about': { prerender: true },
-    '/': { swr: true },
-    '/catalog/**': { swr: 3600 }
+    "/about": { prerender: true },
+    "/": { swr: true },
+    "/catalog/**": { swr: 3600 },
   },
 
-  components:[
+  components: [
     {
-      path: '~/components',
-      pathPrefix: false
-    }
+      path: "~/components",
+      pathPrefix: false,
+    },
   ],
 
-  runtimeConfig:{
+  runtimeConfig: {
     token: "",
     public: {
-      apiurl: ""
-
-    }
+      apiurl: "",
+    },
   },
 
   icon: {
     customCollections: [
       {
         prefix: "icons",
-        dir: "./assets/icons"
-      }
-    ]
+        dir: "./assets/icons",
+      },
+    ],
+  },
+
+  app: {
+    head: {
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css",
+        },
+      ],
+    },
   },
 
   modules: [
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/image',
-    '@nuxt/scripts',
-    '@nuxt/icon'
-  ]
-})
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/image",
+    "@nuxt/scripts",
+    "@nuxt/icon",
+  ],
+});
