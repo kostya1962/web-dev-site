bfb44d63edda2afdcb1cb4f609bd590db184471c
Tuktin Kostya <kostyatuktin@gmail.com>
2025-12-07 01:41:21 +0300

    Повторная фиксация анимации

--- app.vue
+++ app.vue
@@ -5,6 +5,33 @@ import "~/assets/styles/main.css";
 
 <template>
   <NuxtLayout>
-    <NuxtPage />
+    <main class="main-content">
+      <NuxtPage 
+        :transition="{
+          name: 'anim',
+          mode: 'out-in'
+        }" 
+      />
+    </main>
   </NuxtLayout>
 </template>
+
+
+<style scoped>
+  .main-content{
+    margin: 0 auto;
+    padding: 40px 0;
+    max-width: 1248px;
+  }
+
+  .anim-enter-active,
+  .anim-leave-active {
+    transition: all 0.3s ease;
+  }
+
+  .anim-enter-from,
+  .anim-leave-to {
+    opacity: 0;
+    transform: translateY(20px);
+  }
+</style>
